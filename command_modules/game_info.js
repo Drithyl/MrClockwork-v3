dfe8daa3ce0b17c99ada6866a673c58b9138fad5
Drithyl <hugo.bujan@gmail.com>
2019-02-25 12:48:02 -0500

    Added %info command to quickly get the settings and IP/port from a game channel.

---
+++ command_modules/game_info.js
@@ -0,0 +1,47 @@
+
+const config = require("../config.json");
+const permissions = require("../permissions.js");
+const channelFunctions = require("../channel_functions.js");
+const rw = require("../reader_writer.js");
+const regexp = new RegExp(`^${config.prefix}INFO`, "i");
+
+module.exports.enabled = true;
+
+module.exports.gameTypesSupported = [config.dom4GameTypeName, config.dom5GameTypeName];
+
+module.exports.getChannelRequiredToInvoke = "game";
+
+module.exports.getReadableCommand = function()
+{
+  return "info";
+};
+
+module.exports.getCommandArguments = [];
+
+module.exports.getHelpText = function()
+{
+  return `Sends useful game information, like the settings and the IP/port (this one only if you're a player in it).`;
+};
+
+module.exports.isInvoked = function(message, command, args, isDirectMessage, wasSentInGameChannel)
+{
+  if (regexp.test(command) === true && wasSentInGameChannel === true)
+  {
+    return true;
+  }
+
+  else return false;
+};
+
+module.exports.invoke = function(message, command, options)
+{
+  var ipAndPort = "";
+  var info = `${options.game.printSettings().toBox()}`;
+
+  if (member.roles.has(options.game.role.id) === true || permissions.equalOrHigher("gamemaster", member, message.guild.id) === true)
+  {
+    ipAndPort = `The IP for ${options.game.name} is ${options.game.ip}. The port is ${options.game.port}.\n\n`;
+  }
+
+  message.author.send(ipAndPort + info);
+};
