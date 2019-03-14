70d56fa0e665ecc56cafb65925ce4d59c5c0d0ee
Drithyl <hugo.bujan@gmail.com>
2019-03-14 13:23:35 -0400

    Improved error feedback.

--- command_modules/pretender_commands.js
+++ command_modules/pretender_commands.js
@@ -146,7 +146,7 @@ function getSubmittedPretenders(message, game)
   {
     if (err)
     {
-      message.channel.send(`An error occurred when fetching the list of pretenders: ${err}`);
+      message.channel.send(`An error occurred when fetching the list of pretenders:\n\n\t${err}`);
       return;
     }
 
